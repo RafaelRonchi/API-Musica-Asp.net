5d9abb707adee7b8e592e4cbc3059cdf3111a641
Rafael Ronchi <rafael.e01@aluno.ifsc.edu.br>
2023-08-22 13:40:37 -0300

    Exemplo api tabela relacionais

---
+++ API-Musica-Relacionamento/SpotifyAPI/DTOs/MusicaDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace SpotifyAPI.DTOs
+{
+    public class MusicaDTO
+    {
+   
+   
+        public string Titulo { get; set; } = string.Empty;
+        public int AnoLancamento { get; set; } = 0;
+        public int GeneroId { get; set; }
+    }
+}
---
+++ API-Musica-Relacionamento/SpotifyAPI/Data/DataContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpotifyAPI.Models;
+
+namespace SpotifyAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options) { }
+    
+        
+        public DbSet<GeneroModel> Generos { get; set; }
+        public DbSet<MusicaModel> Musicas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            //Mapeamento da relação 1 para N entre genero e musica
+            modelBuilder.Entity<MusicaModel>()
+                .HasOne(m => m.Genero)
+                .WithMany()
+                .HasForeignKey(m=> m.GeneroId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            
+
+
+        }
+
+    }
+}
